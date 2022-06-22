838b8d195728a54e0c94d05b325fd07b10be3d45
Kholod Ihor <kholodihor@gmail.com>
2022-06-22 15:10:53 +0300

    integrate stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -6,8 +6,10 @@ import {
   AiOutlinePlus,
   AiOutlineShopping,
 } from 'react-icons/ai';
+import toast from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
   const cartRef = useRef();
@@ -18,6 +20,29 @@ const Cart = () => {
     toggleCartItemQuanitity,
     onRemove,
   } = useStateContext();
+
+  
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+    
+    const data = await response.json();
+    console.log(data)
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+
+  }
 
   return (
     <div className="Cart__wrapper" ref={cartRef}>
@@ -31,7 +56,7 @@ const Cart = () => {
               <Link href="/" >
                 <Button title={'Continue Shopping'}  />
               </Link>
-            </div>
+            </div>  
           </div>
         )}
 
@@ -87,8 +112,8 @@ const Cart = () => {
                 <span>${totalPrice}.00</span>
               </div>
               <div className="btn-container">
-                <button type="button" className="pay__button">
-                  Pay
+                <button type="button" className="pay__button" onClick={handleCheckout}>
+                  Pay with Stripe
                 </button>
               </div>
             </div>
